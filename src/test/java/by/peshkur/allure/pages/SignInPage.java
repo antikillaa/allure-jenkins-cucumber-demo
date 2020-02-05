f5b1fa2f52848419497dfad6e2432bd9a1a75d03
potato <antikilllaaaa@gmail.com>
2020-02-05 22:19:43 +0300

    fix locators

--- src/test/java/by/peshkur/allure/pages/SignInPage.java
+++ src/test/java/by/peshkur/allure/pages/SignInPage.java
@@ -6,8 +6,8 @@ import org.openqa.selenium.By;
 import static com.codeborne.selenide.Selenide.$;
 
 public class SignInPage {
-    private SelenideElement loginInput = $(By.cssSelector("#form_email123"));
-    private SelenideElement passwordInput = $(By.cssSelector("#form_password123213"));
+    private SelenideElement loginInput = $(By.cssSelector("#form_email"));
+    private SelenideElement passwordInput = $(By.cssSelector("#form_password"));
 
     public void inputLogin(String text) {
         this.loginInput.val(text);
