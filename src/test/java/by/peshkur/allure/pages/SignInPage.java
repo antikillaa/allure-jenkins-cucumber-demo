eea37c34d5e020d9dc9b19fe484e8777de18ae08
potato <antikilllaaaa@gmail.com>
2020-02-05 22:15:59 +0300

    fail tests

--- src/test/java/by/peshkur/allure/pages/SignInPage.java
+++ src/test/java/by/peshkur/allure/pages/SignInPage.java
@@ -6,8 +6,8 @@ import org.openqa.selenium.By;
 import static com.codeborne.selenide.Selenide.$;
 
 public class SignInPage {
-    private SelenideElement loginInput = $(By.cssSelector("#form_email"));
-    private SelenideElement passwordInput = $(By.cssSelector("#form_password"));
+    private SelenideElement loginInput = $(By.cssSelector("#form_email123"));
+    private SelenideElement passwordInput = $(By.cssSelector("#form_password123213"));
 
     public void inputLogin(String text) {
         this.loginInput.val(text);
